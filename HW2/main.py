fc60503f7892b8d3abbad296f4df850dd58e73e6
lewiechiu <louiechiu@gmail.com>
2020-10-22 07:45:17 +0000

    HW2 finalized

--- HW2/main.py
+++ HW2/main.py
@@ -4,6 +4,9 @@ import re
 import numpy as np
 
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
+import argparse
+
 
 
 stopwords = ["a","about","above","after","again","against","ain","all","am","an","and","any","are","aren","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can","couldn","couldn't","d","did","didn","didn't","do","does","doesn","doesn't","doing","don","don't","down","during","each","few","for","from","further","had","hadn","hadn't","has","hasn","hasn't","have","haven","haven't","having","he","her","here","hers","herself","him","himself","his","how","i","if","in","into","is","isn","isn't","it","it's","its","itself","just","ll","m","ma","me","mightn","mightn't","more","most","mustn","mustn't","my","myself","needn","needn't","no","nor","not","now","o","of","off","on","once","only","or","other","our","ours","ourselves","out","over","own","re","s","same","shan","shan't","she","she's","should","should've","shouldn","shouldn't","so","some","such","t","than","that","that'll","the","their","theirs","them","themselves","then","there","these","they","this","those","through","to","too","under","until","up","ve","very","was","wasn","wasn't","we","were","weren","weren't","what","when","where","which","while","who","whom","why","will","with","won","won't","wouldn","wouldn't","y","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves","could","he'd","he'll","he's","here's","how's","i'd","i'll","i'm","i've","let's","ought","she'd","she'll","that's","there's","they'd","they'll","they're","they've","we'd","we'll","we're","we've","what's","when's","where's","who's","why's","would","able","abst","accordance","according","accordingly","across","act","actually","added","adj","affected","affecting","affects","afterwards","ah","almost","alone","along","already","also","although","always","among","amongst","announce","another","anybody","anyhow","anymore","anyone","anything","anyway","anyways","anywhere","apparently","approximately","arent","arise","around","aside","ask","asking","auth","available","away","awfully","b","back","became","become","becomes","becoming","beforehand","begin","beginning","beginnings","begins","behind","believe","beside","besides","beyond","biol","brief","briefly","c","ca","came","cannot","can't","cause","causes","certain","certainly","co","com","come","comes","contain","containing","contains","couldnt","date","different","done","downwards","due","e","ed","edu","effect","eg","eight","eighty","either","else","elsewhere","end","ending","enough","especially","et","etc","even","ever","every","everybody","everyone","everything","everywhere","ex","except","f","far","ff","fifth","first","five","fix","followed","following","follows","former","formerly","forth","found","four","furthermore","g","gave","get","gets","getting","give","given","gives","giving","go","goes","gone","got","gotten","h","happens","hardly","hed","hence","hereafter","hereby","herein","heres","hereupon","hes","hi","hid","hither","home","howbeit","however","hundred","id","ie","im","immediate","immediately","importance","important","inc","indeed","index","information","instead","invention","inward","itd","it'll","j","k","keep","keeps","kept","kg","km","know","known","knows","l","largely","last","lately","later","latter","latterly","least","less","lest","let","lets","like","liked","likely","line","little","'ll","look","looking","looks","ltd","made","mainly","make","makes","many","may","maybe","mean","means","meantime","meanwhile","merely","mg","might","million","miss","ml","moreover","mostly","mr","mrs","much","mug","must","n","na","name","namely","nay","nd","near","nearly","necessarily","necessary","need","needs","neither","never","nevertheless","new","next","nine","ninety","nobody","non","none","nonetheless","noone","normally","nos","noted","nothing","nowhere","obtain","obtained","obviously","often","oh","ok","okay","old","omitted","one","ones","onto","ord","others","otherwise","outside","overall","owing","p","page","pages","part","particular","particularly","past","per","perhaps","placed","please","plus","poorly","possible","possibly","potentially","pp","predominantly","present","previously","primarily","probably","promptly","proud","provides","put","q","que","quickly","quite","qv","r","ran","rather","rd","readily","really","recent","recently","ref","refs","regarding","regardless","regards","related","relatively","research","respectively","resulted","resulting","results","right","run","said","saw","say","saying","says","sec","section","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sent","seven","several","shall","shed","shes","show","showed","shown","showns","shows","significant","significantly","similar","similarly","since","six","slightly","somebody","somehow","someone","somethan","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specifically","specified","specify","specifying","still","stop","strongly","sub","substantially","successfully","sufficiently","suggest","sup","sure","take","taken","taking","tell","tends","th","thank","thanks","thanx","thats","that've","thence","thereafter","thereby","thered","therefore","therein","there'll","thereof","therere","theres","thereto","thereupon","there've","theyd","theyre","think","thou","though","thoughh","thousand","throug","throughout","thru","thus","til","tip","together","took","toward","towards","tried","tries","truly","try","trying","ts","twice","two","u","un","unfortunately","unless","unlike","unlikely","unto","upon","ups","us","use","used","useful","usefully","usefulness","uses","using","usually","v","value","various","'ve","via","viz","vol","vols","vs","w","want","wants","wasnt","way","wed","welcome","went","werent","whatever","what'll","whats","whence","whenever","whereafter","whereas","whereby","wherein","wheres","whereupon","wherever","whether","whim","whither","whod","whoever","whole","who'll","whomever","whos","whose","widely","willing","wish","within","without","wont","words","world","wouldnt","www","x","yes","yet","youd","youre","z","zero","a's","ain't","allow","allows","apart","appear","appreciate","appropriate","associated","best","better","c'mon","c's","cant","changes","clearly","concerning","consequently","consider","considering","corresponding","course","currently","definitely","described","despite","entirely","exactly","example","going","greetings","hello","help","hopefully","ignored","inasmuch","indicate","indicated","indicates","inner","insofar","it'd","keep","keeps","novel","presumably","reasonably","second","secondly","sensible","serious","seriously","sure","t's","third","thorough","thoroughly","three","well","wonder"]
@@ -12,6 +15,14 @@ stemmer = PorterStemmer()
 doc_freq = dict()
 term_freq = dict()
 tfidf = dict()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('doc1', type=str, help='document1')
+    parser.add_argument('doc2', type=str, help='document2')
+
+    args = parser.parse_args()
+    return args
 
 def tokenization(content):
     content = [re.sub(r"[^a-zA-Z0-9]+", '', word) for word in content]
@@ -26,7 +37,7 @@ def read_all_doc():
         for file in files:
             if ".txt" not in file:
                 continue
-            print(os.path.join(root, file))
+            # print(os.path.join(root, file))
             filename = os.path.join(root, file)
             corpus = set()
             with open(filename, "r") as f:
@@ -58,7 +69,7 @@ def calc_tfidf(idf, dict_ind):
         for file in files:
             if ".txt" not in file:
                 continue
-            print(os.path.join(root, file))
+            # print(os.path.join(root, file))
             filename = os.path.join(root, file)
             weight = dict()
             with open(filename, "r") as f:
@@ -83,9 +94,13 @@ def calc_tfidf(idf, dict_ind):
             # return
     pass
 
-def construct_tfidf_vector(freq):
-
-    pass
+def generate_term_index(inv_doc_freq):
+    dictionary_index = {}
+    cnt_ind = 0
+    for word in inv_doc_freq:
+        dictionary_index[word] = cnt_ind
+        cnt_ind += 1
+    return dictionary_index
 
 def cosine(doc1, doc2):
     with open("tfidf/{}.txt".format(doc1), "r") as f:
@@ -93,7 +108,7 @@ def cosine(doc1, doc2):
         content = content[2:]
         
         content = [line.replace("\n", "").split(" ") for line in content]
-        # print(content)
+        
         doc1_vec = np.zeros(14136)
         for line in content:
             ind = line[0]
@@ -101,13 +116,13 @@ def cosine(doc1, doc2):
             doc1_vec[int(ind)] = float(value)
         f.close()
 
-    print(doc1_vec)
+        doc1_vec = normalize(doc1_vec.reshape(1, -1))
 
     with open("tfidf/{}.txt".format(doc2), "r") as f:
         content = f.readlines()
         content = content[2:]
         content = [line.replace("\n", "").split(" ") for line in content]
-        # print(content)
+        
         doc2_vec = np.zeros(14136)
         for line in content:
             
@@ -116,26 +131,25 @@ def cosine(doc1, doc2):
             doc2_vec[int(ind)] = float(value)
         f.close()
 
-    print(doc2_vec)
-    return cosine_similarity(doc1_vec.reshape(1, -1), doc2_vec.reshape(1, -1))
-    pass
-
+        doc2_vec = normalize(doc2_vec.reshape(1, -1))
+    return cosine_similarity(doc1_vec, doc2_vec)
 
 
 if __name__ == "__main__":
+
+    args = parse_args()
+    print("reading all documents...")
     read_all_doc()
     
     doc_freq = {k: v for k, v in sorted(doc_freq.items(), key=lambda item: item[0])}
+    print("writing vector files...")
     write_dictionary(doc_freq)
-    inv_doc_freq = {k: np.log10(1095/ v) for k, v in doc_freq.items() }
-    dictionary_index = {}
-    cnt_ind = 0
-    for word in inv_doc_freq:
-        dictionary_index[word] = cnt_ind
-        cnt_ind += 1
-
+    
+    inv_doc_freq = {k: np.log10((1095)/ (v)) for k, v in doc_freq.items() }
+    dictionary_index = generate_term_index(inv_doc_freq)
+    
     calc_tfidf(inv_doc_freq, dictionary_index)
 
-    print("cosine sim:",cosine(123, 456))
+    print("cosine sim for doc {} and {}:".format(args.doc1, args.doc2),cosine(args.doc1, args.doc2))
 
     pass
--- HW2/report.md
+++ HW2/report.md
@@ -15,8 +15,14 @@ pip install -r requirements.txt
 ```
 
 ## Executing the code
+```bash
+python main.py doc1 doc2 # e.g. python main.py 1 8
 ```
-python main.py
+output
+```
+reading all documents...
+writing vector files...
+cosine sim for doc 1 and 8: [[0.26809512]]
 ```
 * Before running the code, make sure the directory *IRTM* is present.
 * Output will be saved in *dictionary.txt* and *tfidf* folder.
@@ -30,14 +36,21 @@ The program can be broke into several phases.
 
         1. Use *tokenization*, which is from HW1, to preprocess each line of the document.
         2. After we acquired a set of tokens used in the document, update the *doc_freq* dictionary.
-        3. *doc_freq* is a dictionary with *key* being a corpus and *value* being the corpus's document-wise frequency.
+        3. *doc_freq* is a dictionary with *key* being a corpus and *value* being the term's document-wise frequency.
 
 2. Write the *doc_freq* to file and name it *dictionary.txt*.
 3. Calculate the *idf* dictionary given *doc_freq* dictionary.
-    * $idf=\log_{10}\frac{N}{df}$
+    * $$idf(t)=\log_{10}[\frac{N}{df(t)}]$$
 4. For each file:
     1. Use *tokenization* to tokenize the whole document.
     2. For each term, calculate its *tf-idf* score.
     3. Generate a vector with size equal to the dictioanry size, and fill the tfidf scores into the corresponding position of corpus.
 
 ## Cosine Similarity
+
+1. Read the two arguments (*doc1*, *doc2*) from the command line.
+2. Read the vector file from *doc1.txt*, *doc2.txt*.
+3. Construct the tfidf vector from file respectively.
+   1. Fill the value into the tfidf file given in the text vector file.
+   2. Others remain zero if not mentioned.
+4. Compute cosine similarity provided by scikit learn.
